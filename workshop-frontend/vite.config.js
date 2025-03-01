8422241a253385f458a2b82a33c57ec634e06eb3
Carlos Oliveira <carlinhos.oneto0@gmail.com>
2025-03-01 03:12:07 -0300

    chore: ajustes de rotas para o deploy

--- workshop-frontend/vite.config.js
+++ workshop-frontend/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/workshop-frontend/',  // Este deve ser o caminho base do seu repositório GitHub Pages
+  build: {
+    outDir: 'dist',  // Diretório de saída do build
+  },
+});
